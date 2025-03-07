72bdfabc3e2e4db61757757e08f6eb8292eb2014
Martin <36630572+MartinPrograms@users.noreply.github.com>
2025-03-07 15:10:57 +0100

    Begin work on new ui components (drop down & context menu)

--- src/rendering/graphical/ui/components.c
+++ src/rendering/graphical/ui/components.c
@@ -85,3 +85,152 @@ void SpectrumVisualizer(const float *buffer, int bufferSize) {
         });
     }
 }
+
+
+
+void HandleButtonInteraction(__unused Clay_ElementId id, Clay_PointerData data, intptr_t userData){
+    ButtonData *buttonData = (ButtonData *)userData;
+    void (*callback)(void*) = buttonData->callback;
+    void *user_data = buttonData->userData;
+    bool useUserData = buttonData->useUserData;
+
+    if (data.state == CLAY_POINTER_DATA_PRESSED){
+        if (useUserData){
+            callback(user_data);
+        } else {
+            callback(NULL);
+        }
+    }
+}
+
+/// Accepts a lambda function as a callback
+int ButtonCount = 0;
+void CreateButton(const char* text, void* userData, void (*callback)(void*)) {
+    ButtonCount++;
+    Clay_ElementId id = CLAY_IDI("BUTTON", ButtonCount);
+    CLAY({
+             .id = (id), .layout = {.sizing = {CLAY_SIZING_FIT(),
+                                               CLAY_SIZING_FIT()}, .padding = CLAY_PADDING_ALL(STANDARD_PADDING)},
+             .backgroundColor = COLOR_SCHEME_BUTTON,
+             .cornerRadius = CLAY_CORNER_RADIUS(STANDARD_CORNER_RADIUS)
+         }) {
+        ButtonData *dataPtr = arena_alloc(frame_arena, sizeof(ButtonData));
+        dataPtr->text = text;
+        dataPtr->userData = userData;
+        dataPtr->callback = callback;
+        dataPtr->useUserData = userData != NULL;
+        Clay_OnHover(HandleButtonInteraction, (intptr_t)dataPtr);
+        CLAY({
+                 .id = CLAY_ID_LOCAL("ButtonText"), .layout = {.sizing = {CLAY_SIZING_GROW(),
+                                                                          CLAY_SIZING_GROW()},
+                    .padding = CLAY_PADDING_ALL(STANDARD_PADDING)},
+             }) {
+            CLAY_TEXT(GetString(text), CLAY_TEXT_CONFIG(
+                    {.textColor = COLOR_SCHEME_TEXT, .fontSize = 16, .letterSpacing = 0, .wrapMode = CLAY_TEXT_WRAP_NONE}));
+        }
+    }
+}
+
+Clay_String GetString(const char *string) {
+    Clay_String result;
+    result = (CLAY__INIT(Clay_String){.length=strlen(string), .chars=(string)});
+    return result;
+}
+
+void CreateDropdown(int id, const char **items, int itemCount, int selectedIndex, int *outIndex) {
+    // First we need to create a state for the dropdown
+    DropdownState *state = arena_alloc(frame_arena, sizeof(DropdownState));
+    state->isOpen = false;
+    state->selectedIndex = selectedIndex;
+    state->items = items;
+
+    // Now we create the dropdown
+    CLAY({
+             .id = CLAY_IDI("Dropdown", id),
+             .layout = {
+                .sizing = {CLAY_SIZING_FIT(), CLAY_SIZING_FIT()},
+                .padding = CLAY_PADDING_ALL(STANDARD_PADDING),
+                .layoutDirection = CLAY_TOP_TO_BOTTOM
+        },
+             .backgroundColor = COLOR_SCHEME_BACKGROUND_SECONDARY,
+             .cornerRadius = CLAY_CORNER_RADIUS(STANDARD_CORNER_RADIUS)
+         }) {
+        // The dropdown button
+        CLAY({
+                 .id = CLAY_ID_LOCAL("DropdownButton"),
+                 .layout = {
+                    .sizing = {CLAY_SIZING_FIT(), CLAY_SIZING_FIT()},
+                    .padding = CLAY_PADDING_ALL(STANDARD_PADDING),
+                    .layoutDirection = CLAY_LEFT_TO_RIGHT
+            },
+                 .backgroundColor = COLOR_SCHEME_BUTTON,
+                 .cornerRadius = CLAY_CORNER_RADIUS(STANDARD_CORNER_RADIUS)
+             }) {
+            ButtonData *dataPtr = arena_alloc(frame_arena, sizeof(ButtonData));
+            dataPtr->text = items[selectedIndex];
+            dataPtr->userData = state;
+            dataPtr->callback = NULL;
+            dataPtr->useUserData = true;
+            Clay_OnHover(HandleButtonInteraction, (intptr_t) dataPtr);
+            CLAY({
+                     .id = CLAY_ID_LOCAL("DropdownButtonText"),
+                     .layout = {
+                        .sizing = {CLAY_SIZING_GROW(), CLAY_SIZING_GROW()},
+                        .padding = CLAY_PADDING_ALL(STANDARD_PADDING),
+                        .layoutDirection = CLAY_LEFT_TO_RIGHT
+                },
+                 }) {
+                CLAY_TEXT(GetString(items[selectedIndex]), CLAY_TEXT_CONFIG(
+                        {.textColor = COLOR_SCHEME_TEXT, .fontSize = 16, .letterSpacing = 0, .wrapMode = CLAY_TEXT_WRAP_NONE}));
+            }
+        }
+
+        // The dropdown menu
+        if (state->isOpen) {
+            CLAY({
+                     .id = CLAY_ID_LOCAL("DropdownMenu"),
+                     .layout = {
+                        .sizing = {CLAY_SIZING_FIT(), CLAY_SIZING_FIT()},
+                        .padding = CLAY_PADDING_ALL(STANDARD_PADDING),
+                        .layoutDirection = CLAY_TOP_TO_BOTTOM
+                },
+                     .backgroundColor = COLOR_SCHEME_BACKGROUND,
+                     .cornerRadius = CLAY_CORNER_RADIUS(STANDARD_CORNER_RADIUS)
+                 }) {
+                for (int i = 0; i < itemCount; i++) {
+                    CLAY({
+                             .id = CLAY_IDI("DropdownItem", i),
+                             .layout = {
+                                .sizing = {CLAY_SIZING_FIT(), CLAY_SIZING_FIT()},
+                                .padding = CLAY_PADDING_ALL(STANDARD_PADDING),
+                                .layoutDirection = CLAY_LEFT_TO_RIGHT
+                        },
+                             .backgroundColor = COLOR_SCHEME_BUTTON,
+                             .cornerRadius = CLAY_CORNER_RADIUS(STANDARD_CORNER_RADIUS)
+                         }) {
+                        ButtonData *dataPtr = arena_alloc(frame_arena, sizeof(ButtonData));
+                        dataPtr->text = items[i];
+                        dataPtr->userData = state;
+                        dataPtr->callback = NULL;
+                        dataPtr->useUserData = true;
+                        Clay_OnHover(HandleButtonInteraction, (intptr_t) dataPtr);
+                        CLAY({
+                                 .id = CLAY_ID_LOCAL("DropdownItemText"),
+                                 .layout = {
+                                    .sizing = {CLAY_SIZING_GROW(), CLAY_SIZING_GROW()},
+                                    .padding = CLAY_PADDING_ALL(STANDARD_PADDING),
+                                    .layoutDirection = CLAY_LEFT_TO_RIGHT
+                            },
+                             }) {
+                            CLAY_TEXT(GetString(items[i]), CLAY_TEXT_CONFIG(
+                                    {.textColor = COLOR_SCHEME_TEXT, .fontSize = 16, .letterSpacing = 0, .wrapMode = CLAY_TEXT_WRAP_NONE}));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    *outIndex = state->selectedIndex;
+}
+
--- src/rendering/graphical/ui/components.h
+++ src/rendering/graphical/ui/components.h
@@ -7,10 +7,25 @@ typedef struct {
     const char* text;
     void* userData;
     void (*callback)(void*);
+    bool useUserData;
 } ButtonData;
+
+typedef struct {
+    bool isOpen;
+    int selectedIndex;
+    const char** items;
+} DropdownState;
 
 extern int CustomComponentIndex;
 void WaveformVisualizer(const float* buffer, int bufferSize);
 void SpectrumVisualizer(const float* buffer, int bufferSize);
+
+extern int ButtonCount;
+void CreateButton(const char *text, void* userData, void (*callback)(void*));
+
+void CreateDropdown(int id, const char** items, int itemCount, int selectedIndex, int* outIndex);
+void CreateContextMenu(const char** items, int itemCount, void (*callback)(int));
+
+Clay_String GetString(const char *string);
 
 #endif
